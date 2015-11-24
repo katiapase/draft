a0e506f9e3e5d961af99d8b03465f11b4a8660b7
katiapase <katiapase@katiapamacbook3.lan>
2015-11-24 16:26:27 +1100

    draft of my final project

---
+++ myscript.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	$('#GDS3501').click(function() {
+		$('#MainProductImage').attr('src', 'images/GDS35/GDS_35_2.jpg')
+        // alert($('#MainProductImage').attr('src'));
+	});
+	
+	$('#GDS3502').click(function() {
+		$('#MainProductImage').attr('src', 'images/GDS35/GDS_35_4.jpg')
+         // alert($('#MainProductImage').attr('src'));
+	});
+	
+	$('#GDS3503').click(function() {
+		$('#MainProductImage').attr('src', 'images/GDS35/GDS_35_6.jpg')
+         // alert($('#MainProductImage').attr('src'));
+	});
+	
+	$('#GDS3504').click(function() {
+		$('#MainProductImage').attr('src', 'images/GDS35/GDS_35_8.jpg')
+         // alert($('#MainProductImage').attr('src'));
+	});
+	
+	$('#GDS3505').click(function() {
+		$('#MainProductImage').attr('src', 'images/GDS35/GDS_35_10.jpg')
+         // alert($('#MainProductImage').attr('src'));
+	});
+});
+
+// $(document).ready(function() {
+
+// $(function(){
+
+//     var main = $('#MainProductImage');//get the object once
+
+//     //target is imag tag, where id starts with `sm00`
+//     $('img[id^=GDS350]').on('click', function(){
+//         attr('src', this.src);
+//     });
+// });
+// });
+
+  
